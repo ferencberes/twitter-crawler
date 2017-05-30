925cbc8767659b33670181db9c18a24d821d05c9
Ferenc Béres <ferdzso05@gmail.com>
2017-05-30 16:10:32 +0200

    crawled messages are pushed to mongodb collection

--- python/request_scheduler.py
+++ python/request_scheduler.py
@@ -1,15 +1,46 @@
 import time, datetime
 import numpy as np
 from collections import deque
+from pymongo import MongoClient
 
 class RequestScheduler():
-    def __init__(self,time_frame,max_requests,verbose=False):
+    def __init__(self,time_frame,max_requests,port=27017,db_name="twitter-crawler",verbose=False):
         """Abstract scheduler object. It enables only 'max_requests' requests in every 'time_frame' seconds."""
+        # scheduler parameters
         self.time_frame = time_frame
         self.max_requests = max_requests
         self.verbose = verbose
         self._requests = deque([])
-        self._crawled_data = {}
+        # mongodb parameters
+        self._client, self._db = None, None
+        self._raw_coll = None
+        self._port = port
+        self._db_name = db_name
+        
+    def connect(self,collection_name):
+        """Connect to MongoDB collection"""
+        try:
+            self._client = MongoClient('mongodb://localhost:%i/' % self._port)
+            self._db = self._client[self._db_name]
+            try:
+                self._db.create_collection(collection_name)
+                print("'%s' collection was created!" % collection_name)
+            except:
+                pass
+            self._raw_coll = self._db[collection_name]
+            print("Connection was created successfully!")
+        except:
+            raise
+        
+    def close(self):
+        """Close MongoDB connection"""
+        try:
+            if self._client != None:
+                self._client.close()
+        
+            print("Connection was closed successfully!")
+        except:
+            raise
         
     def execute_request(self):
         return None
@@ -30,7 +61,7 @@ class RequestScheduler():
             # request
             if len(self._requests) < self.max_requests:
                 time_str = time.strftime("%b %d %Y %H:%M:%S",time.localtime())
-                self._crawled_data[time_str] = self.execute_request()
+                self._raw_coll.insert_one({"time":time_str,"result":self.execute_request()})
                 self._requests.append(time.time())
                 wait_for = np.random.normal(loc=delta_t,scale=delta_t*dev_ratio)
                 if self.verbose:
@@ -47,6 +78,3 @@ class RequestScheduler():
                         print("Sleeping for %.1f seconds" % sync_time)
                     time.sleep(sync_time)
                     
-    def get(self):
-        """Return results collected"""
-        return self._crawled_data

9a4994ece5fbc37599ddbba94a27c0d3d4a1929a
Ferenc Béres <ferdzso05@gmail.com>
2017-05-30 15:35:52 +0200

    run with search parameters and feedback time

--- python/request_scheduler.py
+++ python/request_scheduler.py
@@ -1,27 +1,41 @@
-import time
+import time, datetime
 import numpy as np
 from collections import deque
 
 class RequestScheduler():
-    def __init__(self,time_frame,max_requests):
+    def __init__(self,time_frame,max_requests,verbose=False):
+        """Abstract scheduler object. It enables only 'max_requests' requests in every 'time_frame' seconds."""
         self.time_frame = time_frame
         self.max_requests = max_requests
+        self.verbose = verbose
         self._requests = deque([])
         self._crawled_data = {}
         
     def execute_request(self):
         return None
         
-    def run(self,delta_t,dev_ratio=0.2,sync_time=10):
+    def run(self,delta_t,dev_ratio=0.2,sync_time=10, feedback_time=15*60):
+        """Run scheduler object. It will sleep for normally distributed seconds after each request.
+        'delta_t' is the mean and 'delta_t'*'dev_ratio' is the deviance of this normal distribution."""
+        start_time, last_feedback = time.time(), time.time()
         print("Started at: %s\n" % time.strftime("%b %d %Y %H:%M:%S",time.localtime()))
         while True:
+            # feedback
+            current_time = time.time()
+            if current_time - last_feedback > feedback_time:
+                td = datetime.timedelta(seconds=current_time - start_time)
+                print("Scheduler RUNNING since: %i days, %i hours and %i minutes" % (td.days, td.seconds//3600, (td.seconds//60)%60))
+                last_feedback = current_time
+                
+            # request
             if len(self._requests) < self.max_requests:
                 time_str = time.strftime("%b %d %Y %H:%M:%S",time.localtime())
                 self._crawled_data[time_str] = self.execute_request()
-                print("A REQUEST was made!")
                 self._requests.append(time.time())
                 wait_for = np.random.normal(loc=delta_t,scale=delta_t*dev_ratio)
-                print("Sleeping for %.1f seconds" % wait_for)
+                if self.verbose:
+                    print("A REQUEST was made!")
+                    print("Sleeping for %.1f seconds" % wait_for)
                 time.sleep(wait_for)
             else:
                 time_diff = time.time() - self._requests[0]
@@ -29,8 +43,10 @@ class RequestScheduler():
                     self._requests.popleft()
                     continue
                 else:
-                    print("Sleeping for %.1f seconds" % sync_time)
+                    if self.verbose:
+                        print("Sleeping for %.1f seconds" % sync_time)
                     time.sleep(sync_time)
                     
     def get(self):
+        """Return results collected"""
         return self._crawled_data
--- python/twitter_crawler.py
+++ python/twitter_crawler.py
@@ -4,10 +4,12 @@ from request_scheduler import *
 
 class TwitterCrawler(RequestScheduler):
     def __init__(self,time_frame,max_requests):
+        """Twitter API scheduler object. It enables only 'max_requests' requests in every 'time_frame' seconds."""
         super(TwitterCrawler, self).__init__(time_frame,max_requests)
         self.twitter_api = None
         
     def authenticate(self,auth_file_path):
+        """Authenticate application with Twython."""
         try:
             with open(auth_file_path,"r") as f:
                 auth_info = json.load(f)
@@ -15,12 +17,22 @@ class TwitterCrawler(RequestScheduler):
             print("Authentication was successful!")
         except:
             raise
+            
+    def set_search_arguments(self,search_args):
+        """Set search parameters with a dictionary"""
+        self.search_args = search_args
+        print(self.search_args)
         
     def execute_request(self):
         search_results = None
         try:
-            search_results = self.twitter_api.search(q='#FINABudapest2017', result_type='recent', count=20)
+            if self.search_args != None:
+                search_results = self.twitter_api.search(**self.search_args)
+            else:
+                raise RuntimeError("You must set search arguments!")
         except TwythonError as e:
             print(e)
+        except:
+            raise
         finally:
             return search_results

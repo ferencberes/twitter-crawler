1f212a0c83244b99c9fdca322cf1c32f94a88534
Beres Ferenc <ferdzso05@gmail.com>
2017-10-20 15:25:29 +0200

    moving some cleaning and preprocessing functions to script

---
+++ python/rg17/text_cleaning.py
@@ -0,0 +1,45 @@
+import snowballstemmer, re
+
+def clean_text(t):
+    """Remove non-alphabetical characters + remove commans and numbers"""
+    # get only alphabetical words + full mentions
+    clean_1 = ' '.join(re.findall("[\w,\@,']+",t))
+    # remove numbers and comma
+    clean_2 = ' '.join(re.findall("[^\,,\d]+",clean_1))
+    return clean_2.lower()
+
+def get_words_above_size_limit(t, size_limit):
+    """Get list of words above the provided size limit"""
+    words = []
+    for w in t.split():
+        if len(w) > size_limit:
+            words.append(w)
+    return words
+
+class CustomStemmer:
+    def __init__(self, fixed_words, lang):
+        self._stemmer = snowballstemmer.stemmer(lang)
+        self.fixed_words = [w.lower() for w in fixed_words]
+        
+    def stem_word(self, word):
+        do_stem = True
+        if "@" in word:
+            do_stem = False
+        elif "#" in word:
+            do_stem = False
+        else:
+            do_stem = not word.lower() in self.fixed_words
+        return self._stemmer.stemWords([word])[0] if do_stem else word
+    
+    def remove_hashtag(self, word):
+        has_hashtag = False
+        if "#" == word[0]:
+            has_hashtag = True
+        return word[1:] if has_hashtag else word
+        
+    def stem_words(self, words, remove_hashtag=False):
+        if remove_hashtag:
+            return [self.remove_hashtag(self.stem_word(w)) for w in words]
+        else:
+            return [self.stem_word(w) for w in words]
+        

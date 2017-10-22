1820b989b139da65bca76c69ffe637ecd6fbb2b8
Beres Ferenc <ferdzso05@gmail.com>
2017-10-22 15:15:14 +0200

    w2v evaluated with renamed former models

--- python/rg17/evaluate_toplist.py
+++ python/rg17/evaluate_toplist.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import json
+import gensim, os, json
 
 def transform_account_name(acc_name, remove_digits, remove_under_score, to_lower):
     result = acc_name
@@ -31,3 +31,41 @@ def get_toplist_with_max_scores(pair_occs_df, key_words, snapshot_ids, score_col
     If more than 1 snapshot id is specified then only the occurances with maximum score is kept from he duplicated items."""
     df = get_toplist(pair_occs_df, key_words, snapshot_ids, score_col=score_col)
     return df.groupby(by=["word_1","word_2"])[score_col].max().reset_index().sort_values(score_col, ascending=False)
+
+
+### Word2Vec ###
+
+def load_w2v_models(w2v_model_dir):
+    """Load w2v models trained for snapshots"""
+    model_names = os.listdir(w2v_model_dir)
+    w2v_models = {}
+    for m in model_names:
+        snapshot_id = m.split(".")[0]
+        w2v_models[snapshot_id] = gensim.models.Word2Vec.load("%s/%s" % (w2v_model_dir, m))
+    return w2v_models
+
+def w2v_query(w2v_models, key_word, snapshot_id, top_k=None):
+    """Handle w2v model request for only one key word and snapshot id."""
+    out = pd.DataFrame()
+    try:
+        model = w2v_models[snapshot_id]
+        if top_k == None:
+            top_k = len(model.wv.vocab)
+        if key_word in  model.wv.vocab:
+            res = model.most_similar(positive=[key_word], topn=top_k)
+        else:
+            res = []
+        out = pd.DataFrame(res, columns=["word_2","w2v_score"])
+        out["word_1"] = key_word
+        out["time"] = snapshot_id
+        out = out[["time","word_1","word_2","w2v_score"]]
+    except:
+        raise
+    finally:
+        return out
+
+def get_w2v_toplist(w2v_models, key_words, snapshot_ids, top_k):
+    """Get most similar words based on word2vec. 
+    If more than 1 snapshot id is specified then there could be duplications in the data!"""
+    dfs = [w2v_query(w2v_models, kw, sid, top_k) for kw in key_words for sid in snapshot_ids]
+    return pd.concat(dfs).sort_values("w2v_score", ascending=False)

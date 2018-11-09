b53e4eff353ac7a8d21ad1522c1761d1f158886b
Beres Ferenc <ferdzso05@gmail.com>
2018-11-09 11:19:39 +0100

    save old changes for text cleaning rg17

--- python/rg17/text_cleaning.py
+++ python/rg17/text_cleaning.py
@@ -1,56 +1,134 @@
-from nltk.stem.snowball import SnowballStemmer
-import re
+import contractions, nltk, unicodedata, re
+from nltk.stem import SnowballStemmer, WordNetLemmatizer
 import numpy as np
-import pandas as pd
 
-### Text Cleaning ### 
+### Text Cleaning ###
 
-def clean_text(t, pattern="[\w,\@,',\#]+"):
-    """Remove non-alphabetical characters + remove commas"""
-    # get only alphabetical words + full mentions
-    clean_1 = ' '.join(re.findall(pattern,t))
-    # remove comma
-    clean_2 = ' '.join(re.findall("[^\,]+",clean_1))
-    return clean_2.lower()
+def replace_contractions(text, verbose=False):
+    """Replace contractions in string of text"""
+    new_text = contractions.fix(text)
+    if verbose:
+        print(new_text)
+    return new_text
 
-def get_words_above_size_limit(t, size_limit):
-    """Get list of words above the provided size limit"""
-    words = []
-    for w in t.split():
-        if len(w) > size_limit:
-            words.append(w)
-    return words
-
-
-### Stemming ###
-
-class CustomStemmer:
-    def __init__(self, fixed_words, lang):
-        self._stemmer = SnowballStemmer(lang)
-        self.fixed_words = [w.lower() for w in fixed_words]
-        
-    def stem_word(self, word):
-        do_stem = True
-        if "@" in word:
-            do_stem = False
-        elif "#" in word:
-            do_stem = False
+def tokenization_and_lowercase(text, verbose=False):
+    "Handle tokenization and lowercasing for words"
+    words = nltk.word_tokenize(text)
+    i = 0
+    fixed_words = []
+    while i < len(words):
+        if words[i] == "@":
+            # no lowercasing for mentions
+            if i != len(words) - 1:
+                fixed_words.append("@"+words[i+1])
+            i += 2
         else:
-            do_stem = not word.lower() in self.fixed_words
-        return self._stemmer.stem(word) if do_stem else word
-    
-    def remove_hashtag(self, word):
-        has_hashtag = False
-        if "#" == word[0]:
-            has_hashtag = True
-        return word[1:] if has_hashtag else word
-        
-    def stem_words(self, text, remove_hashtag=True, pattern="[\w,\@,',\#]+"):
-        words = clean_text(text, pattern=pattern).split()
-        if remove_hashtag:
-            return [self.remove_hashtag(self.stem_word(w)) for w in words]
+            fixed_words.append(words[i].lower())
+            i += 1
+    if verbose:
+        print(fixed_words)
+    return fixed_words
+
+def remove_non_ascii(words, verbose=False):
+    """Remove non-ASCII characters from list of tokenized words"""
+    new_words = []
+    for word in words:
+        new_word = unicodedata.normalize('NFKD', word).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+        new_words.append(new_word)
+    if verbose:
+        print(new_words)
+    return new_words
+
+def remove_numbers(words, verbose=False):
+    """Remove numbers from tokenized words"""
+    new_words = []
+    for word in words:
+        if not word.isdigit():
+            new_words.append(word)
+    if verbose:
+        print(new_words)
+    return new_words
+
+def remove_short_words(words, limit=1, verbose=False):
+    """Remove short words from tokenized words"""
+    new_words = []
+    for word in words:
+        if len(word) > limit:
+            new_words.append(word)
+    if verbose:
+        print(new_words)
+    return new_words
+
+
+def remove_punctuation(words, verbose=False):
+    """Remove punctuation from list of tokenized words. Mentions are enabled (due to Twitter text)"""
+    new_words = []
+    for word in words:
+        new_word = re.sub(r'[^\w\s\@]', '', word)
+        if new_word != '':
+            new_words.append(new_word)
+    return new_words
+
+def remove_stopwords(words, stop_words, verbose=False):
+    """Remove stop words from list of tokenized words"""
+    new_words = []
+    for word in words:
+        if word not in stop_words:
+            new_words.append(word)
+    if verbose:
+        print(new_words)
+    return new_words
+
+def stem_words(words, fixed_words=[], verbose=False):
+    """Stem words in list of tokenized words. Do NOT stem mentions!"""
+    stemmer = SnowballStemmer('english')
+    stems = []
+    for word in words:
+        if "@" in word or word in fixed_words:
+            stem = word
         else:
-            return [self.stem_word(w) for w in words]
+            stem = stemmer.stem(word)
+        stems.append(stem)
+    if verbose:
+        print(stems)
+    return stems
+
+def lemmatize_words(words, fixed_words=[], verbose=False):
+    """Lemmatize words in list of tokenized words"""
+    lemmatizer = WordNetLemmatizer()
+    lemmas = []
+    for word in words:
+        if word in fixed_words:
+            lemma = word
+        else:
+            # lemmatize as noun
+            lemma = lemmatizer.lemmatize(word,pos='n')
+            # lemmatize as verb
+            lemma = lemmatizer.lemmatize(lemma,pos='v')
+        lemmas.append(lemma)
+    if verbose:
+        print(lemmas)
+    return lemmas
+
+def process_tweet_text(text, stop_words, fixed_words=[], use_stemming=False, verbose=False):
+    """Clean and normalize Tweet text."""
+    # format
+    text = text.replace('\n',' ')
+    text = replace_contractions(text, verbose=verbose)
+    words = tokenization_and_lowercase(text, verbose=verbose)
+    # cleaning
+    cleaned_words = remove_non_ascii(words, verbose=verbose)
+    cleaned_words = remove_punctuation(cleaned_words, verbose=verbose)
+    cleaned_words = remove_stopwords(cleaned_words, stop_words, verbose=verbose)
+    cleaned_words = remove_numbers(cleaned_words, verbose=verbose)
+    cleaned_words = remove_short_words(cleaned_words, limit=2, verbose=verbose)
+    # normalize
+    if use_stemming:
+        stemmed_words = stem_words(cleaned_words, fixed_words=fixed_words, verbose=verbose)
+        return " ".join(stemmed_words)
+    else:
+        lemmatized_words = lemmatize_words(cleaned_words, fixed_words=fixed_words, verbose=verbose)
+        return " ".join(lemmatized_words)
 
 ### Cleaning Combined Occurences ###
 

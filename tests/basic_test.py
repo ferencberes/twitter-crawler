ae4ca44f0f00cdd8f96d892844d61831ab2280fc
Ferenc Beres <ferdzso05@gmail.com>
2021-05-31 16:42:53 +0200

    github actions first steps 3

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -3,11 +3,12 @@ import os, sys
 from twittercrawler.crawlers import *
 from twittercrawler.utils import load_json_result
 
-api_key_file_path = "../api_key.json"
+dirpath = os.path.dirname(os.path.realpath(__file__))
+api_key_file_path = os.path.join(dirpath, "..", "api_key.json")
 
 def test_api_key():
     os.path.exists(api_key_file_path)
-    
+"""    
 def test_recursive():
     crawler = RecursiveCrawler(limit=2)
     crawler.authenticate(api_key_file_path)
@@ -60,7 +61,7 @@ def test_lookup():
     crawler.close()
     os.remove("lookup_results.txt")
     assert cnt > 0
-
+"""
 def test_friends():
     crawler = FriendsCollector(limit=1)
     crawler.authenticate(api_key_file_path)
@@ -69,3 +70,4 @@ def test_friends():
     crawler.close()
     os.remove("friends_results.txt")
     assert cnt > 0
+

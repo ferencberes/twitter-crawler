00a223b11d03146ab80c8c405a9f3342684430ff
Ferenc Beres <ferdzso05@gmail.com>
2021-06-02 14:49:55 +0200

    masking data keys during io

--- tests/io_test.py
+++ tests/io_test.py
@@ -4,12 +4,16 @@ sample_tweets = [
     {
         "id_str":"199",
         "full_text":"abc",
-        "user":{"screen_name":"john doe"}
+        "lang":"en",
+        "user":{"screen_name":"john doe"},
+        "created_at": "Wed Jun 02 11:43:27 +0000 2021"
     },
     {
         "id_str":"200",
         "full_text":"def",
-        "user":{"screen_name":"jane doe"}
+        "lang":"es",
+        "user":{"screen_name":"jane doe"},
+        "created_at": "Wed Jun 02 11:45:33 +0000 2021"
     },
 ]
 test_file = "log.txt"
@@ -29,8 +33,27 @@ def test_file_io_again():
     reader = FileReader(test_file)
     df = reader.read()
     assert len(df) == 4
+    assert df.shape[1] == 5
+    
+def test_file_io_include():
+    writer = FileWriter(test_file, clear=True, include_mask=["id_str","lang","full_text"])
+    writer.write(sample_tweets)
+    writer.close()
+    reader = FileReader(test_file)
+    df = reader.read()
+    assert len(df) == 2
     assert df.shape[1] == 3
     
+def test_file_io_exclude():
+    writer = FileWriter(test_file, clear=True, exclude_mask=["user"])
+    writer.write(sample_tweets)
+    writer.close()
+    reader = FileReader(test_file)
+    df = reader.read()
+    assert len(df) == 2
+    assert df.shape[1] == 4
+    
+"""    
 def test_kafka_io():
     topic, host, port = "sample", "localhost", 9092 
     success = False
@@ -48,4 +71,4 @@ def test_kafka_io():
     except:
         raise
     assert success
-    
+"""
--- twittercrawler/data_io.py
+++ twittercrawler/data_io.py
@@ -5,8 +5,31 @@ from .utils import load_json_result
 
 ### Writers ###
 
-class FileWriter():
-    def __init__(self, file_path, clear=False):
+#TODO: mention, retweet mask
+
+class Writer():
+    def __init__(self, include_mask=None, exclude_mask=None):
+        self._include_mask = include_mask
+        self._exclude_mask = exclude_mask
+        
+    def _prepare_record(self, record):
+        if self._include_mask != None:
+            rec = {}
+            for key in self._include_mask:
+                rec[key] = record[key]
+        else:
+            rec = record.copy()
+            if self._exclude_mask != None:
+                for key in self._exclude_mask:
+                    del rec[key]
+        return json.dumps(rec)
+        
+    def write(self, results):
+        pass
+
+class FileWriter(Writer):
+    def __init__(self, file_path, clear=False, include_mask=None, exclude_mask=None):
+        super(FileWriter, self).__init__(include_mask, exclude_mask)
         if clear or not os.path.exists(file_path):
             self._output_file = open(file_path, 'w')
         else:
@@ -14,13 +37,14 @@ class FileWriter():
             
     def write(self, results):
         for res in results:
-            self._output_file.write("%s\n" % json.dumps(res))
+            self._output_file.write("%s\n" % self._prepare_record(res))
             
     def close(self):
         self._output_file.close()
         
-class KafkaWriter():
-    def __init__(self, topic, host="localhost", port=9092):
+class KafkaWriter(Writer):
+    def __init__(self, topic, host="localhost", port=9092, include_mask=None, exclude_mask=None):
+        super(KafkaWriter, self).__init__(include_mask, exclude_mask)
         self.host = host
         self.port = port
         self.topic = topic
@@ -29,7 +53,7 @@ class KafkaWriter():
     def write(self, results):
         for res in results:
             key_b = res["id_str"].encode("utf-8")
-            value_b = json.dumps(res).encode("utf-8")
+            value_b = self._prepare_record(res).encode("utf-8")
             self._producer.send(self.topic, key=key_b, value=value_b)
             
     def close(self):
